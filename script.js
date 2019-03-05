11945621a6cc49daf6b03d191bbe4d9d70ea4803
Reception 01 <hmgrec01@HMGE.LOCAL>
2019-03-05 05:22:06 +0100

    Style initial setup

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 const hourHand = document.querySelector("hour-hand");
 const minHand = document.querySelector("min-hand");
 const secHand = document.querySelector("second-hand");
-
-secHand.addEventListener("mouseover", console.log("TEST"));
-
-function setDate(){
-const dateNow = new Date();
-}
-
