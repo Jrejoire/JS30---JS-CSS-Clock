cf654ea4d2732de69df7fb00ab21364f7df538b2
jrej <jrejoire@hotmail.fr>
2019-03-05 21:05:32 +0100

    Time functionality set up

--- script.js
+++ script.js
@@ -4,38 +4,23 @@ const secHand = document.querySelector(".second-hand");
 
 function initialLocalTime()
 {
-	let date = new Date;
-	let seconds = date.getSeconds();
-	let minutes = date.getMinutes();
-	let hours =	date.getHours();
+	const date = new Date();
 
-	let hands = [
-		{ 
-			hand: 'hour',
-			angle: (hours*30) +(minutes/2)
-		},
-		{
-			hand: 'min',
-			angle:(minutes*6),
-		},
-		{
-			hand:'sec',
-			angle:(seconds*6),
-		}
-	];
-	for (let i = 0; i< hands.length; i ++)
-	{	
-		let elements = hands[i].hand + 'Hand';	
+	const seconds = date.getSeconds();
+	const secondsdegree = ((seconds/60)*360) + 90;
+	secHand.style.transform = `rotateZ(${secondsdegree}deg)`;
+	
+	const minutes = date.getMinutes();
+	const minutesdegree = ((minutes/60)*360)+((seconds/60)*6) + 90;
+	minHand.style.transform = `rotateZ(${minutesdegree}deg)`;
 
-		for (let j = 0; j<elements.length; j++)
-		{
-			elements[j].style.webkitTransform ='rotateZ(' + hands[i].angle + 'deg)';
-			elements[j].style.transform = 'rotateZ(' + hands[i].angle + 'deg)';
-			if (hands[i].hand === 'min')
-			{
-				elements[i].parentNode.setAttribute('data-second-angle',hands[i+1].angle);
-			}
-		}
-	}
+	const hours = date.getHours();
+	const hoursdegree = (hours/12)*360 + ((minutes/60)*30) + 90;
+	hourHand.style.transform = `rotateZ(${hoursdegree}deg)`;
 
 }
+
+setInterval(initialLocalTime, 1000);
+
+
+initialLocalTime();
